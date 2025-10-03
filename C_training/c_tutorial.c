4c3875d2ae5fb9ab1ac3e396b362b1f3d64fa38c
ghmg3929 <ghmg3929@leeds.ac.uk>
2025-10-03 15:39:40 +0000

    2

--- C_training/c_tutorial.c
+++ C_training/c_tutorial.c
@@ -3,7 +3,7 @@
 int main(void) {
   // Define an array of 100 characters.
   char buffer[100];
-  // Define a pointer to a chracter and point it
+  // Define a pointer to a character and point it
   // to the first character in the buffer.
   char* current_char = buffer;
   // Define an array of 20 points to characters and set
@@ -18,18 +18,22 @@ int main(void) {
     // If the current character is a space, a new line,
     // or a tab, coming after an ordinary character
     // terminate the string by placing '\0' in the current
-    // location of buffer.
 
     // PLACE YOUR CODE HERE
+    if (current_char == " "){
+      current_char = '\0';
+      prev_char_not_ordinary = 1;
+    }
 
     // If the current character is an ordinary character
     // after a special character save the pointer to it in
     // the words[] array.
     else if (prev_char_not_ordinary) {
-      // PLACE YOUR CODE HERE
+      words[i] = &current_char;
+      prev_char_not_ordinary = 0;
     }
     // Move the pointer in buff by one place.
-    // PLACE YOUR CODE HERE
+    *current_char++;
   }
   *current_char = '\0';
   printf("\n");
